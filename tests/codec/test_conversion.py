7a75382e77ac23bdbefffbb7aef260c74e29d73d
Shintaro Shiba <shiba.shintaro@gmail.com>
2023-03-08 23:05:21 +0100

    Adding test

---
+++ tests/codec/test_conversion.py
@@ -0,0 +1,6 @@
+from evlib.codec import fileformat
+
+# TODO setup small test data
+# ev_iter = fileformat.IteratorTextEvent('example_event.txt')
+# data_keys = {"x": "raw_events/x", "y": "raw_events/y", "t": "raw_events/t", "p": "raw_events/p"}
+# fileformat.convert_iterator_access_to_hdf5(ev_iter, 'example_event.hdf5', data_keys)

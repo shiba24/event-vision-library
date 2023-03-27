58432ca6ea7bb2a59dc2252ce16bd71984da1c72
shiba24 <shiba.shintaro@gmail.com>
2023-03-27 16:33:12 +0200

    Update tests

--- tests/codec/test_conversion.py
+++ tests/codec/test_conversion.py
@@ -1,6 +1,9 @@
 from evlib.codec import fileformat
 
 
+def test_mock():
+    pass
+
 # TODO setup small test data
 # ev_iter = fileformat.IteratorTextEvent('example_event.txt')
 # data_keys = {"x": "raw_events/x", "y": "raw_events/y", "t": "raw_events/t", "p": "raw_events/p"}

8a6b4d0f7eba3a3f842ee6005619be9d57d30359
Shintaro Shiba <shiba.shintaro@gmail.com>
2023-04-16 10:58:35 +0200

    fix test

--- tests/preproc/test_hot_pixel.py
+++ tests/preproc/test_hot_pixel.py
@@ -6,15 +6,15 @@ from evlib.preproc import hot_pixel
 
 
 
-def test_hot_pixel_none():
+def test_hot_pixel_none() -> None:
     ne = 500
     height, width = 20, 40
     ev = basic_utils.generate_events(ne, height, width, 0.1, 0.24)
     hot_pixel_filtered = hot_pixel.hot_pixel_filter(ev, (height, width), 500)
-    assert len(ev) == len(hot_pixel_filtered)
+    assert len(hot_pixel_filtered) == ne
 
 
-def test_hot_pixel_all():
+def test_hot_pixel_all() -> None:
     ne = 500
     height, width = 20, 40
     ev = basic_utils.generate_events(ne, height, width, 0.1, 0.24)

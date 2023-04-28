0d2b8af7228df5a9df5ca4a42e4799a15bf32c4b
Friedhelm Hamann <f.hamann@posteo.net>
2023-04-28 12:31:49 +0200

    basic unit tests representations

--- tests/processing/reconstruction/test_e2vid.py
+++ tests/processing/reconstruction/test_e2vid.py
@@ -4,11 +4,10 @@ from evlib.processing.reconstruction import E2Vid
 import torch
 
 
-def test_run_e2vid():    # type: ignore
+def test_build_e2vid_shape():    # type: ignore
     num_events = 500
     height, width = 20, 40
     events = basic_utils.generate_events(num_events, height, width, 0.1, 0.24)
     reconstructor = E2Vid((height, width), use_gpu=torch.cuda.is_available())
-    # TODO implement me!
-    # image = reconstructor(events)
-    # assert image.shape == (height, width)
+    image = reconstructor(events)
+    assert image.shape == (height, width)

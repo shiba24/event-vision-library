50e8c2f709fcc1d59de3855f1b92a96f25e9d368
agirbau <andreu.girbau@gmail.com>
2023-09-20 13:22:29 +0900

    Added bin file iterator.

---
+++ src/evlib/codec/fileformat/bin.py
@@ -0,0 +1,91 @@
+"""Bin format.
+Used in N-Caltech101, N-cars.
+"""
+import logging
+from typing import Any
+
+import numpy as np
+
+
+logger = logging.getLogger(__name__)
+
+from ...types import RawEvents
+from ._iterator_access import IteratorAccess
+
+
+# TODO make parser abstract and merge these classes.
+
+
+class IteratorBin(IteratorAccess):
+    FORMAT = "bin"
+
+    def __init__(self, binfile: str) -> None:
+        # TODO add parse format option
+        super().__init__(binfile)
+        # Variables
+        self.size_x = None
+        self.size_y = None
+        # Initialize variables
+        self.file = open(self.file_name, 'rb')
+        self._init_vars_()
+
+    def __iter__(self) -> Any:
+        self.count = 0
+        return self
+
+    def _init_vars_(self):
+        # Read event file
+        self.raw_evs = self.read_ev_file()
+        # Estimate width and height of sensor given the events in the stream
+        self.size_x = self.raw_evs[:, 0].max() + 1
+        self.size_y = self.raw_evs[:, 1].max() + 1
+
+    def read_ev_file(self):
+        # From https://github.com/gorchard/event-Python/blob/master/eventvision.py#L532
+        raw_data = np.fromfile(self.file, dtype=np.uint8)
+        self.file.close()
+        raw_data = np.uint32(raw_data)
+        raw_evs = self._transform_raw_to_evs_(raw_data)
+        return raw_evs
+
+    @staticmethod
+    def _transform_raw_to_evs_(raw_data):
+        all_y = raw_data[1::5]
+        all_x = raw_data[0::5]
+        all_p = (raw_data[2::5] & 128) >> 7  # bit 7
+        all_ts = ((raw_data[2::5] & 127) << 16) | (raw_data[3::5] << 8) | (raw_data[4::5])
+
+        # Process time stamp overflow events
+        time_increment = 2 ** 13
+        overflow_indices = np.where(all_y == 240)[0]
+        for overflow_index in overflow_indices:
+            all_ts[overflow_index:] += time_increment
+
+        # Everything else is a proper td spike
+        td_indices = np.where(all_y != 240)[0]
+
+        x_ = all_x[td_indices]
+        y_ = all_y[td_indices]
+        t_ = all_ts[td_indices]
+        p_ = all_p[td_indices]
+
+        raw_evs = np.vstack([x_, y_, t_, p_]).T
+        return raw_evs
+
+
+class IteratorBinEvent(IteratorBin):
+    def __next__(self) -> RawEvents:
+        """
+        Returns:
+            RawEvents: events
+        """
+        raw_evs = self.raw_evs
+        _l = len(raw_evs)
+
+        x = raw_evs[:, 0].astype(np.int32)
+        y = raw_evs[:, 1].astype(np.int32)
+        t = raw_evs[:, 2].astype(np.float64)
+        p = raw_evs[:, 3].astype(bool)
+
+        self.count += _l
+        return RawEvents(x=x, y=y, timestamp=t, polarity=p)
